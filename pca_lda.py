596dfdff61bd99383bcebdab09aa011f037d5c79
Daniel R. King <58912239+daniel-r-king@users.noreply.github.com>
2022-06-13 12:37:41 -0400

    Add files via upload

--- pca_lda.py
+++ pca_lda.py
@@ -6,6 +6,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 import pandas as pd
 
 random_numbers = np.random.randint(0, 10000, size = (900, 5))
@@ -25,8 +26,8 @@ lda_training_model = LinearDiscriminantAnalysis(n_components = 2)
 training_data = lda_training_model.fit_transform(training_data, training_classes)
 test_data = lda_training_model.transform(test_data)
 
-lda_df = pd.DataFrame(data = training_data, index = training_classes)
-pca_df = pd.DataFrame(data = training_data_pcs, index = training_classes)
+lda_df = pd.DataFrame(data = training_data, index = training_classes, columns = ['ld1', 'ld2'])
+pca_df = pd.DataFrame(data = training_data_pcs, index = training_classes, columns = ['pc1', 'pc2'])
 
 print(lda_df.head(n = 50))
 print(pca_df.head(n = 50))
@@ -46,3 +47,19 @@ class_prediction = classifier.predict(test_data)
 cm = confusion_matrix(test_classes, class_prediction)
 
 print('PCA model accuracy = ' + str(accuracy_score(test_classes, class_prediction)))
+
+colors = {'Class 1': 'red', 'Class 2':'green', 'Class 3':'blue'}
+
+lda_plot = plt.figure(1)
+plt.scatter(lda_df['ld1'], lda_df['ld2'], c = lda_df.index.map(colors))
+plt.xlabel('ld1')
+plt.ylabel('ld2')
+
+pca_plot = plt.figure(2)
+plt.scatter(pca_df['pc1'], pca_df['pc2'], c = pca_df.index.map(colors))
+plt.xlabel('pc1')
+plt.ylabel('pc2')
+
+plt.show()
+
+
